01a68e27b83952b440b82fc31408a2a1677dbc55
TheLegendary1248 <1248alex@gmail.com>
2022-04-13 20:41:10 -0400

    Created 'center'

---
+++ input.js
@@ -0,0 +1,12 @@
+//This script handles the keyboard inputs
+const input = 
+{ 
+    downKeys: [], 
+    justHitKeys: [],
+    justReleasedKeys: [],
+    getKeyDown(event)
+    {
+        this.downKeys
+    }
+}
+document.addEventListener()
